0d4a50a8920b06196ce0b738a63e8a6586caa78e
jsoo <jisoo2718@gmail.com>
2023-01-06 16:24:08 +0900

    프로그래머스 level3

---
+++ programmers/python/43164.py
@@ -0,0 +1,20 @@
+import collections
+
+def solution(tickets):
+    graph = collections.defaultdict(list)
+    for a, b in sorted(tickets):
+        graph[a].append(b)
+
+    answer = []
+
+    def dfs(a):
+        while graph[a]:
+            dfs(graph[a].pop(0))
+        answer.append(a)
+
+    dfs('ICN')
+
+    return answer[::-1]
+
+answer = solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]])
+print(answer)
