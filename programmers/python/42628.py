119d7eaa35284d60f40ba88b63fb06d6bf2dc763
jsoo <jisoo2718@gmail.com>
2023-01-07 10:58:38 +0900

    프로그래머스 level 3

---
+++ programmers/python/42628.py
@@ -0,0 +1,25 @@
+import heapq
+
+def solution(operations):
+    answer = []
+    heap = []
+    
+    for operation in operations:
+        item = operation.split()
+        if item[0] == 'I':
+            heapq.heappush(heap, int(item[1]))
+        elif item[0] == 'D' and heap:
+            if item[1] == '1':
+                heap.remove(max(heap))
+            elif item[1] == '-1':
+                heapq.heappop(heap)
+
+    if heap:
+        answer = [max(heap), heap[0]]
+    else:
+        answer = [0,0]
+
+    return answer
+
+answer = solution(["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"])
+print(answer)
